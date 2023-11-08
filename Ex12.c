95b7e502f73498650f7fd5f10d63eeb377e05fd9
Daniel Correia <130627373+Danielprogrammer01@users.noreply.github.com>
2023-11-08 20:42:34 -0300

    Ex12.c

--- Ex12.c
+++ Ex12.c
@@ -1,10 +1,8 @@
-/*ExercÌcio 12: 
-Desenvolva um programa que solicite ao usu·rio uma quantidade ]
-de notas e peÁa para digitar cada nota. 
-O programa deve calcular e exibir a mÈdia das notas digitadas. 
-Utilize o for para repetir o processo de solicitaÁ„o das notas.*/
-
-
+/*Exerc√≠cio 12: 
+Desenvolva um programa que solicite ao usu√°rio uma quantidade ]
+de notas e pe√ßa para digitar cada nota. 
+O programa deve calcular e exibir a m√©dia das notas digitadas. 
+Utilize o for para repetir o processo de solicita√ß√£o das notas.*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,23 +16,23 @@ int main()
 
     setlocale(LC_ALL, "portuguese");
 
-float nota;
-int contador = 0, i;
-float soma, media;
+    float nota;
+    int contador = 0, i, quantidadeNotas;
+    float soma, media;
 
-for (i = 1; i <= 4; i++)
-{
-printf("Insira a %d∞ nota: \n", i);
-scanf("%f", &nota);
+    printf("Insira a quantidade de notas que deseja: \n");
+    scanf("%d", &quantidadeNotas);
+    for (i = 1; i <= quantidadeNotas; i++)
+    {
+        printf("Insira a %d¬∞ nota: \n", i);
+        scanf("%f", &nota);
 
-contador++;
-soma += nota;
+        contador++;
+        soma += nota;
+    }
 
-}
+    media = soma / contador;
+    printf("A m√©dia dos valores √© %.2f \n", media);
 
-media = soma/contador;
-printf("A mÈdia dos valores È %.2f \n", media);
-
-
-return 0;
+    return 0;
 }
